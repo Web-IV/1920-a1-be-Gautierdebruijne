84576726d3a4fd131ccdf2ea9e34df6f1ca8ae08
Gautier de Bruijne <gautier.debruijne.y7024@student.hogent.be>
2020-03-05 18:54:13 +0100

    Add NSwag

--- Recipe API/Recipe API/Startup.cs
+++ Recipe API/Recipe API/Startup.cs
@@ -26,6 +26,7 @@ namespace Recipe_API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerDocument();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +38,9 @@ namespace Recipe_API
             }
 
             app.UseHttpsRedirection();
+
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
 
             app.UseRouting();
 
