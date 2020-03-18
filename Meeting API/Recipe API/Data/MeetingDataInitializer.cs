dde6a4fe0e3cb2b2ea2615b84273189bce32e111
Gautier de Bruijne <gautier.debruijne.y7024@student.hogent.be>
2020-03-18 23:03:09 +0100

    Add MeetingDataInitializer

---
+++ Meeting API/Recipe API/Data/MeetingDataInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipe_API.Data
+{
+    public class MeetingDataInitializer
+    {
+        private readonly MeetingContext _context;
+
+        public MeetingDataInitializer(MeetingContext context)
+        {
+            _context = context;
+        }
+
+        public void InitializeData()
+        {
+            _context.Database.EnsureDeleted();
+            if (_context.Database.EnsureCreated())
+            {
+                //seeding
+            }
+        }
+    }
+}
